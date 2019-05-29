6963e44fbb1ef5b267b505532863e267d43b67d5
crcrpar <masaki.kozuki.2014@gmail.com>
2019-05-29 21:16:46 +0900

    fix: give -> given

--- errors.go
+++ errors.go
@@ -6,5 +6,5 @@ var (
 	errDimensionMismatch         = errors.New("dimension mismatch")
 	errInvalidIndex              = errors.New("invalid index")
 	errInvalidKeyVector          = errors.New("invalid key vector")
-	errItemNotFoundOnGivenItemID = errors.New("item not found for give item id")
+	errItemNotFoundOnGivenItemID = errors.New("item not found for given item id")
 )
